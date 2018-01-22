d1f8cfa289c7c7d4ec6e0280ba1bea9c7859efc3
Sam Jeston <sam.jeston@gmail.com>
2018-01-22 16:58:30 +1000

    ABX-4051 Functional deployments to the Ropsten Network

--- truffle.js
+++ truffle.js
@@ -1,3 +1,9 @@
+// Ropsten MetaMask nmonic
+const ropstenNmonic = 'base impulse among dog dignity exercise gas piece split people honey choose'
+
+// https://github.com/trufflesuite/truffle-hdwallet-provider
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -6,6 +12,11 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(ropstenNmonic, 'https://ropsten.infura.io/'),
+      network_id: 3,
+      gas: 2900000
     }
   }
 };
