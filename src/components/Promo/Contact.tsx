f68832cacd154ca3e313b76c59703c3383126d4d
Carl Foster <carl.john.foster@gmail.com>
2018-02-01 11:58:31 +1000

    Make things nice

--- src/components/Promo/Contact.tsx
+++ src/components/Promo/Contact.tsx
@@ -1,66 +1,59 @@
 import * as React from 'react'
 
-export class Contact extends React.Component<null, null> {
-  constructor(props: any) {
-    super(props)
-  }
-
-  public render() {
-
-    return (
-      <div>
-        <section className='contact ptb-100 bg-off' id='contact'>
-          <div className='container'>
-            <div className='section_title mb-70 text-center'>
-              <h2>Register Your Interest</h2>
-              <p>Submit your information below and you'll be added to the Kinesis mailing list.</p>
-            </div>
-            {/* End of .section_title */}
-            <div className='row'>
-              <div className='col-sm-6'>
-                  <form className='quform' method='post' encType='multipart/form-data'>
-                    <div className='row quform-elements'>
-                      <div className='quform-input form-group col-sm-12'>
-                        <label className='sr-only'>First Name</label>
-                        <input type='text' className='form-control' name='fname' defaultValue='' placeholder='First Name *' required />
-                      </div>
-                      {/* End of .form-group */}
-                      <div className='quform-input form-group col-sm-12'>
-                        <label className='sr-only'>Last Name</label>
-                        <input type='text' className='form-control' name='lname' defaultValue='' placeholder='Last Name *' required />
-                      </div>
-                      {/* End of .form-group */}
-                      <div className='quform-input form-group col-sm-12'>
-                        <label className='sr-only'>Email</label>
-                        <input type='email' className='form-control' name='email' defaultValue='' placeholder='Email Address *' required />
-                      </div>
-                      <div className='col-sm-12 mt-20 mb-20'>
-                        <button type='submit' className='btn '>Submit</button>
-                      </div>
-                      {/* End of .col-sm-12 */}
-                    </div>
-                    {/* End of .row */}
-                  </form>
-                </div>
-                {/* End of .content */}
-              {/* End of .col-sm-8 */}
-              <div className='col-sm-4'>
-                <div className='widget text-left mb-40 pl-20'>
-                  <div className='mb-10'>
-                    <h5 className='text-uppercase'>Head Office</h5>
+export const Contact: React.SFC = () => {
+  return (
+    <div>
+      <section className='services pt-80 pt-80' id='contact'>
+        <div className='container'>
+          <div className='section_title mb-70 text-center'>
+            <h2>Register Your Interest</h2>
+            <p>Submit your information below and you'll be added to the Kinesis mailing list.</p>
+          </div>
+          {/* End of .section_title */}
+          <div className='row'>
+            <div className='col-sm-6'>
+              <form className='quform' method='post' encType='multipart/form-data'>
+                <div className='row quform-elements'>
+                  <div className='quform-input form-group col-sm-12'>
+                    <label className='sr-only'>First Name</label>
+                    <input type='text' className='form-control' name='fname' defaultValue='' placeholder='First Name *' required />
                   </div>
-                  <address>
-                    <p><span>address:</span> 1600 Amphitheatre Parkway, Mountain View, CA, 94043</p>
-                    <p><span>phone:</span> (+000) 1252 000522</p>
-                    <p><span>email:</span> index@email.com</p>
-                  </address>
+                  {/* End of .form-group */}
+                  <div className='quform-input form-group col-sm-12'>
+                    <label className='sr-only'>Last Name</label>
+                    <input type='text' className='form-control' name='lname' defaultValue='' placeholder='Last Name *' required />
+                  </div>
+                  {/* End of .form-group */}
+                  <div className='quform-input form-group col-sm-12'>
+                    <label className='sr-only'>Email</label>
+                    <input type='email' className='form-control' name='email' defaultValue='' placeholder='Email Address *' required />
+                  </div>
+                  <div className='col-sm-12 mt-20 mb-20'>
+                    <button type='submit' className='btn '>Submit</button>
+                  </div>
+                  {/* End of .col-sm-12 */}
                 </div>
+                {/* End of .row */}
+              </form>
+            </div>
+            {/* End of .content */}
+            {/* End of .col-sm-8 */}
+            <div className='col-sm-4'>
+              <div className='widget text-left mb-40 pl-20'>
+                <div className='mb-10'>
+                  <h5 className='text-uppercase'>Head Office</h5>
+                </div>
+                <address>
+                  <p><span>address:</span> 1600 Amphitheatre Parkway, Mountain View, CA, 94043</p>
+                  <p><span>phone:</span> (+000) 1252 000522</p>
+                  <p><span>email:</span> index@email.com</p>
+                </address>
               </div>
-              </div>
-              </div>
-              {/* End of .col-sm-4 */}
-            </section>
-      </div>
-    )
-  }
+            </div>
+          </div>
+        </div>
+        {/* End of .col-sm-4 */}
+      </section>
+    </div>
+  )
 }
--- src/components/Promo/Intro.tsx
+++ src/components/Promo/Intro.tsx
@@ -1,28 +1,20 @@
 import * as React from 'react'
 
-export class Intro extends React.Component<null, null> {
-  constructor(props: any) {
-    super(props)
-  }
-
-  public render() {
-
-    return (
-      <div>
-        <section className='startup pt-50 pb-20 bg-off'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-sm-8'>
-                <h3 className='mb-10'>Why Kinesis</h3>
-                <p className='mb-20'>Kinesis is redefining physically backed cryptocurrencies.</p>
-              </div>
-              <div className='col-sm-4 text-right'>
-                <a href='#' className='btn mt-10 wow fadeInRight'>Whitepaper</a>
-              </div>
+export const Intro: React.SFC = () => {
+  return (
+    <div>
+      <section className='startup'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-sm-9'>
+              <h4 style={{color: 'black', lineHeight: '50px'}}><strong>Why Kinesis?</strong> Kinesis is redefining physically backed cryptocurrencies.</h4>
+            </div>
+            <div className='col-sm-3'>
+              <a href='#' className='btn wow fadeInRight' style={{marginBottom: 0, border: '2px solid black', backgroundColor: 'transparent'}}>Whitepaper</a>
             </div>
           </div>
-        </section>
-      </div>
-    )
-  }
+        </div>
+      </section>
+    </div>
+  )
 }
