0d76ed6237dbcf0392ad290ac1b832980d66e268
Adriaan Prinsloo <adriaan.prisnloo@gmail.com>
2018-02-12 16:47:27 +0100

    ABX-4073 Change Home text

--- src/components/Promo/Home.tsx
+++ src/components/Promo/Home.tsx
@@ -9,10 +9,11 @@ export const Home: React.SFC = () => {
       <section className='banner pt-80 style_2' data-stellar-background-ratio='.7' id='home'>
         <div className='overlay' />
         <div className='container' style={{textAlign: 'center'}}>
-          <h2 className='mb-10 light_color'>A Revolutionary Cryptocurrency</h2>
-          <h2 className='mb-10 light_color' style={{ fontWeight: 'bold', marginTop: '-28px' }}>where each coin</h2>
-          <h2 className='mb-10 light_color' style={{ fontWeight: 'bold', marginTop: '-28px' }}>is backed by <span className='primary_color'>physical gold</span></h2>
-          <ScrollLink style={{ paddingLeft: '12px', paddingRight: '12px' }} className='ml-5 mr-5 mb-20 btn prehighlight' to='contact' spy={true} smooth={true} offset={0} duration={500}>Register You Interest</ScrollLink>
+          <h2 className='mb-10 light_color'>Kinesis</h2>
+          <h3 className='mb-10 light_color'>More than a revolutionary crypto currency</h3>
+          <h3 className='mb-10 light_color'>More than a <span className='primary_color'>gold backed</span> coin</h3>
+          <h3 className='mb-10 light_color'>Kinesis is the future of money, the crypto banking system for the people of the digital age</h3>
+          <ScrollLink style={{ paddingLeft: '12px', paddingRight: '12px' }} className='ml-5 mr-5 mb-20 btn prehighlight' to='contact' spy={true} smooth={true} offset={0} duration={500}>Register Your Interest</ScrollLink>
         </div>
       </section>
     </div>
