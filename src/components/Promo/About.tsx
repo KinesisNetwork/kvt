73dbc48afad1f83510d002c5948fb98a0d60bc2e
Adriaan Prinsloo <adriaan.prisnloo@gmail.com>
2018-02-12 17:30:43 +0100

    Shortened About section

--- src/components/Promo/About.tsx
+++ src/components/Promo/About.tsx
@@ -13,8 +13,6 @@ export class About extends React.Component<null, null> {
           <div className='container'>
             <div className='section_title text-center mb-60'>
               <h2>What does Kinesis offer?</h2>
-              <p>Imagine this: a multifaceted yield system, empowering users to be financially rewarded based on their participation and the overall velocity (rate that money changes hands) of the Kinesis currencies. This concept will change the way the world views physical precious metals. It’s a revolutionary idea giving users a unique yield, derived purely from economic output rather than debt like fiat currency with fractional banking.</p>
-              <p>To take maximum advantage of the Kinesis potential for return, you must register for the Kinesis Velocity Token sale. Only holders of the Kinesis Velocity Token will be eligible to share in transaction fees associated with all Kinesis Currencies.</p>
             </div>
             {/* End of .section_title */}
             <div className='row text-center m-0'>
@@ -22,7 +20,7 @@ export class About extends React.Component<null, null> {
                 <div className='content'>
                   <img src='images/website-assets/01Security.png' />
                   <h3>Security</h3>
-                  <p>Kinesis has been built on tried and tested, industry leading blockchain platforms. Using the Ethereum network for the Kinesis Velocity Token and Stellar for the Kinesis Currency, coupled with an industry leading exchange platform and firmly established world-class vaulting and independent auditing networks, the Kinesis Suite of Products have been designed with security and trust in mind.</p>
+                  <p>Block chain technology, combined with our industry leading precious metals exchange and a firmly established global vaulting network ensures security in the Kinesis suite.</p>
                 </div>
                 {/* End of .content */}
               </div>
@@ -30,8 +28,8 @@ export class About extends React.Component<null, null> {
               <div className='col-xs-6 col-sm-4 border-right'>
                 <div className='content'>
                   <img src='images/website-assets/03Backed.png' />
-                  <h3>Initial Token Offering</h3>
-                  <p>The Kinesis Velocity Token (KVT) is an ERC20 Token. This Token enables the holders to share in transaction fees associated with the Kinesis suite of currencies. It goes without saying how attractive a substantial share in the transactional fees of a high velocity currency, backed by physical precious metals could be.</p>
+                  <h3>Stability</h3>
+                  <p>The physical backing of Kinesis by precious metal creates stability in a marketplace plagued by volatility.</p>
                 </div>
                 {/* End of .content */}
               </div>
@@ -39,8 +37,8 @@ export class About extends React.Component<null, null> {
               <div className='col-xs-6 col-sm-4'>
                 <div className='content'>
                   <img src='images/website-assets/02Integration.png' />
-                  <h3>Physically Backed</h3>
-                  <p>The Kinesis suite of currencies are backed by 100% physical precious metals. The first of the digital currencies to launch will be Kinesis Gold whereby each and every digital coin will be backed by 1g of physical gold bullion with a fineness of 9999. Stay tuned for additional details about the Kinesis Silver coin. The metals held in legally binding trust, undergo a comprehensive bi-annual independent audit, so you know your precious metals are the highest in quality, are beneficially owned by Kinesis coin holders and are 100% genuine.</p>
+                  <h3>Scarcity</h3>
+                  <p>The number of Kinesis coins on offer are limited to the amount of pure bullion there is in the world.</p>
                 </div>
                 {/* End of .content */}
               </div>
--- src/components/Promo/What.tsx
+++ src/components/Promo/What.tsx
@@ -13,8 +13,9 @@ export class What extends React.Component<null, null> {
           <div className='container'>
             <div className='section_title text-center mb-60'>
               <h2>What is Kinesis</h2>
-              <p>Kinesis is a revolutionary digital currency, secured by physical bullion, made dynamic by a crypto banking system, supercharged by transactional velocity, and for the first time in history, providing a mutli-faceted yield system for those who utilise it.</p>
-              <p>Kinesis is a unique concept giving users direct access to a unique multifaceted yield system, empowering users to be financially rewarded based on the global use of this currency. This concept will change the way the world views digital currencies and enforces the security a currency should provide.</p>
+              <h3>Kinesis is a revolutionary digital currency, secured by physical bullion, made dynamic by a crypto banking system, supercharged by transactional velocity, and for the first time in history, providing a multi-faceted yield system for those who utilise it.</h3>
+              <p>Kinesis is a revolutionary concept giving users direct access to a unique multifaceted yield system, empowering users to be financially rewarded based on the global use of this currency. This concept will change the way the world views digital currencies and enforces the security a currency should provide.</p>
+              <p>The Kinesis gold and silver suite of currencies are backed 1 to 1 by 100% physical precious metals. Physical precious metal is brought into trust, simultaneously with coin minting, to ensure full backing 100% of the life of each coin.</p>
             </div>
             {/* End of .section_title */}
           </div>
