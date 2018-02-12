7449fc0a26017df178890392f4c0718d130513e7
Adriaan Prinsloo <adriaan.prisnloo@gmail.com>
2018-02-12 16:53:07 +0100

    Add space above button.

--- src/components/Promo/Home.tsx
+++ src/components/Promo/Home.tsx
@@ -13,7 +13,9 @@ export const Home: React.SFC = () => {
           <h3 className='mb-10 light_color'>More than a revolutionary crypto currency</h3>
           <h3 className='mb-10 light_color'>More than a <span className='primary_color'>gold backed</span> coin</h3>
           <h3 className='mb-10 light_color'>Kinesis is the future of money, the crypto banking system for the people of the digital age</h3>
-          <ScrollLink style={{ paddingLeft: '12px', paddingRight: '12px' }} className='ml-5 mr-5 mb-20 btn prehighlight' to='contact' spy={true} smooth={true} offset={0} duration={500}>Register Your Interest</ScrollLink>
+          <div style={{paddingTop: '30px'}}>
+          <ScrollLink style={{ paddingLeft: '12px', paddingRight: '12px'}} className='ml-5 mr-5 mb-20 btn prehighlight' to='contact' spy={true} smooth={true} offset={0} duration={500}>Register Your Interest</ScrollLink>
+          </div>
         </div>
       </section>
     </div>
