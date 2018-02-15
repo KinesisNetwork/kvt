64f23f501a20beb29f4090099e34a93769d7dff2
Joel Olsson <joel_1st@hotmail.com>
2018-02-15 20:16:02 +1000

    ABX-4073 add other team members

--- src/components/Promo/Team.tsx
+++ src/components/Promo/Team.tsx
@@ -16,10 +16,10 @@ export class Team extends React.Component<null, null> {
               <h2>A Team With a Wealth of Experience</h2>
             </div>
             <div className='row'>
-              <div className='col-sm-4 col-xs-6'>
+              <div className='col-sm-4 col-xs-12'>
                 <div className='content'>
                   <div className='img_container'>
-                    <img src='images/team/1.png' alt='team' className='img-responsive' />
+                    <img src='images/team/1.png' alt='team' className='img-responsive' style={{maxWidth: '200px'}}/>
                     <h3 className='team_info wow fadeInUp'>
                       Thomas Coughlin  
                     </h3>
@@ -31,10 +31,10 @@ export class Team extends React.Component<null, null> {
                   </div>
                 </div>
               </div>
-              <div className='col-sm-4 col-xs-6'>
+              <div className='col-sm-4 col-xs-12'>
                 <div className='content'>
                   <div className='img_container'>
-                    <img src='images/team/2.png' alt='team' className='img-responsive' />
+                    <img src='images/team/2.png' alt='team' className='img-responsive' style={{maxWidth: '200px'}}/>
                     <h3 className='team_info wow fadeInUp'>
                       Michael Coughlin
                     </h3>
@@ -46,10 +46,10 @@ export class Team extends React.Component<null, null> {
                   </div>
                 </div>
               </div>
-              <div className='col-sm-4 col-xs-6'>
+              <div className='col-sm-4 col-xs-12'>
                 <div className='content'>
                   <div className='img_container'>
-                    <img src='images/team/3.png' alt='team' className='img-responsive' />
+                    <img src='images/team/3.png' alt='team' className='img-responsive' style={{maxWidth: '200px'}}/>
                     <h3 className='team_info wow fadeInUp'>
                       David Charles 
                     </h3>
@@ -64,10 +64,10 @@ export class Team extends React.Component<null, null> {
               </div>
             </div>
             <div className='row'>
-              <div className='col-sm-4 col-xs-6'>
+              <div className='col-sm-4 col-xs-12'>
                 <div className='content'>
                   <div className='img_container'>
-                    <img src='images/team/4.png' alt='team' className='img-responsive' />
+                    <img src='images/team/4.png' alt='team' className='img-responsive' style={{maxWidth: '200px'}}/>
                     <h3 className='team_info wow fadeInUp'>
                       David Underwood
                     </h3>
@@ -79,10 +79,10 @@ export class Team extends React.Component<null, null> {
                   </div>
                 </div>
               </div>
-              <div className='col-sm-4 col-xs-6'>
+              <div className='col-sm-4 col-xs-12'>
                 <div className='content'>
                   <div className='img_container'>
-                    <img src='images/team/5.png' alt='team' className='img-responsive' />
+                    <img src='images/team/5.jpg' alt='team' className='img-responsive' style={{maxWidth: '200px'}}/>
                     <h3 className='team_info wow fadeInUp'>
                      Rosalyn Charlton
                     </h3>
@@ -94,10 +94,10 @@ export class Team extends React.Component<null, null> {
                   </div>
                 </div>
               </div>
-              <div className='col-sm-4 col-xs-6'>
+              <div className='col-sm-4 col-xs-12'>
                 <div className='content'>
                   <div className='img_container'>
-                    <img src='images/team/6.png' alt='team' className='img-responsive' />
+                    <img src='images/team/6.jpg' alt='team' className='img-responsive' style={{maxWidth: '200px'}}/>
                     <h3 className='team_info wow fadeInUp'>
                      Eric Maine
                     </h3>
@@ -111,47 +111,64 @@ export class Team extends React.Component<null, null> {
           </div>
           </div>
             <div className='row'>
-              <div className='col-sm-4 col-xs-6'>
+              <div className='col-sm-6 col-xs-12'>
                 <div className='content'>
                   <div className='img_container'>
-                    <img src='images/team/4.png' alt='team' className='img-responsive' />
+                    <img src='images/team/7.png' alt='team' className='img-responsive' style={{maxWidth: '200px'}} />
                     <h3 className='team_info wow fadeInUp'>
-                      David Underwood
+                      Ben Brideaux
                     </h3>
-                    <span className='team_title'>Non-Executive Director</span>
-                    <p>David Underwood graduated as a teacher from the University of Southern Queensland in 1974 and worked with the Queensland Department of Education until 1977.  During his time in Queensland Parliament he was the Shadow Minister of Health, Education, Tourism and National Parks serving numerous Parliamentary Committees.  Following his resignation from Parliament, he was a consultant to business and industry for a number of years prior to becoming the Mayor of Ipswich City for four years until local government amalgamation of Ipswich City and Moreton Shire in 1995.  David has been a shareholder in ABX since its inception, and brings a wealth of experience and policy expertise to the Board </p>
+                    <span className='team_title'>Global Project Manager</span>
+                      <p>Ben is the Compliance specialist and Global Project Manager, ably managing the programs of work across all projects which constitute the Kinesis ecosystem Ben works closely with the business divisions across 4 continents, working closely with the CTO to ensure project delivery and system development alignment.</p>
                     <DisplayMore style={{fontWeight: 'bold'}}>
-                      <p> Following his resignation from Parliament, he was a consultant to business and industry for a number of years prior to becoming the Mayor of Ipswich City for four years until local government amalgamation of Ipswich City and Moreton Shire in 1995.  David has been a shareholder in ABX since its inception, and brings a wealth of experience and policy expertise to the Board </p>
+                      <p>A lawyer by trade, Ben additionally brings to the team a wealth of experience in Legal Compliance, with specialist knowledge in the precious metals market along with the cryptocurrency domain.  Ben oversees global identity verification and anti-money laundering implementations, ensuring industry standard compliance is met on all global client facing processes.</p>
                     </DisplayMore>
                   </div>
                 </div>
               </div>
-              <div className='col-sm-4 col-xs-6'>
+              <div className='col-sm-6 col-xs-12'>
                 <div className='content'>
                   <div className='img_container'>
-                    <img src='images/team/5.png' alt='team' className='img-responsive' />
+                    <img src='images/team/8.png' alt='team' className='img-responsive' style={{maxWidth: '200px'}} />
                     <h3 className='team_info wow fadeInUp'>
-                      Rosalyn Charlton
+                      Richard Melbourne
                     </h3>
-                    <span className='team_title'>Chief Technology Officer</span>
-                    <p>Rosalyn Charlton is the Chief Technology Officer (CTO) of Allocated Bullion Exchange (ABX). As an Agile Practitioner, she has coached and led sizeable teams of professionals and directed numerous successful projects using Agile strategies. </p>
+                    <span className='team_title'>Head of Member Services</span>
+                    <p>Richard Melbourne is Head of Member Services at Allocated Bullion Exchange (ABX). Richard has spent the past decade within the Financial Services industry as asenior investment advisor and head of operations at both boutique specialty investment firms, along with internationally respected investment houses. Richard has always been in customer facing roles and understands thesignificant value of delivering excellence in customer experience.</p>
                     <DisplayMore style={{fontWeight: 'bold'}}>
-                      <p>Rosalyn is a resourceful and insightful professional with a natural disposition to strategic problem solving using both her extensive knowledge of Agile methodologies and her inherent problem solving skills. Applying these skills through strategic architectures and technologies, Rosalyn ensures that business objectives are always fully comprehended and met. Her areas of expertise include IT Strategy, Strategic Management, Agile Development and Management Methodologies, SCRUM, Kanban. Enterprise Architecture Principles, Development Team Leadership and Management, Effective Team Dynamics, Collaborative Management, Change Management, Business Process Design and Analysis.</p>
+                      <p>Richard holds a Bachelor of Business, majoring in both management and finance from Griffith University and has also studied international markets abroad at The University of Sheffield in the UK. His experience building relationships with institutional and sophisticated clients, each fromhighly varying industries, underpins the unrivaled level of customer satisfaction he aims to deliver in this role. </p>
                     </DisplayMore>
                   </div>
                 </div>
               </div>
-              <div className='col-sm-4 col-xs-6'>
+          </div>
+            <div className='row'>
+              <div className='col-sm-6 col-xs-12'>
                 <div className='content'>
                   <div className='img_container'>
-                    <img src='images/team/6.png' alt='team' className='img-responsive' />
+                    <img src='images/team/9.jpg' alt='team' className='img-responsive' style={{maxWidth: '200px'}} />
                     <h3 className='team_info wow fadeInUp'>
-                      Eric Maine
+                      Dan Ryan 
                     </h3>
-                    <span className='team_title'>Chief Strategy Officer</span>
-                    <p>Eric Maine has more than 30 years of senior management experience in the exchange and financial markets. His most recent positions in the Asia Pacific Region included Director of Market Development at the Hong Kong Mercantile Exchange (HKMEx), Head of Product Development and Management at Singapore Exchange (SGX), and Head of Product & Research at Singapore Mercantile Exchange (SMX). Eric has also held positions at Intercontinental Exchange (ICE) and the New York Board of Trade. </p>
+                    <span className='team_title'>General Council</span>
+                      <p>Dan Ryan is a commercial and technology lawyer with over 15 years’ high-level advisory and transactional experience in Asia. Mr Ryan began his career working for leading international law firms (Baker & McKenzie, Freshfields) in their corporate, intellectual property and technology groups before moving to senior in-house roles.</p>
                     <DisplayMore style={{fontWeight: 'bold'}}>
-                      <p>Outside of the exchange space, Eric has held senior level management positions, including Senior Managing Director of Canning/Maine Inc, a New York based investment firm, Managing Director/Head of Metals at Allied Irish Banks and Vice President of Metals and Mining at Prudential-Bache (now Jefferies Bache).</p>
+                      <p>He has served as a director on a range of boards – from publicly-listed multinationals, to fast-moving technology start-ups. In addition to his legal background, he reads and speaks Mandarin to a very high professional level, and has particular expertise in the China market.</p>
+                    </DisplayMore>
+                  </div>
+                </div>
+              </div>
+              <div className='col-sm-6 col-xs-12'>
+                <div className='content'>
+                  <div className='img_container'>
+                    <img src='images/team/10.jpg' alt='team' className='img-responsive' style={{maxWidth: '200px'}} />
+                    <h3 className='team_info wow fadeInUp'>
+                      Peter Magan
+                    </h3>
+                    <span className='team_title'>Operations Manager</span>
+                    <p>Peter is a highly experienced financial services professional with extensive knowledge of trading, operations and systems for multiple financial instruments and products in theEuropean, North American and Asian markets.</p>
+                    <DisplayMore style={{fontWeight: 'bold'}}>
+                      <p>With over 25 years of wide ranging business experience, Peter held avariety of Back and Middle Office positions with top tier financial institutions in London before moving to Singapore to head up the establishment of regional Asia Pacific Futures Middle Offices, firstly for MF Global and then ICAP.</p>
                     </DisplayMore>
                   </div>
                 </div>
