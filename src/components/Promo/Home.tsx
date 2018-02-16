a83ca0e1ba68143ed6bbe410606fd72abd545dc1
Joel Olsson <joel_1st@hotmail.com>
2018-02-17 08:26:29 +1000

    change title name

--- src/components/Promo/Home.tsx
+++ src/components/Promo/Home.tsx
@@ -18,9 +18,8 @@ export class Home extends React.Component<null, null> {
           <div className='overlay' id='particles-js'/>
           <div className='container' style={{textAlign: 'center'}}>
             <h1 className='mb-10 light_color' style={{fontWeight: 'bold'}}>Kinesis</h1>
-            <h2 className='hero-text mb-10 light_color'>More than a revolutionary crypto currency</h2>
-            <h2 className='hero-text mb-10 light_color'>More than a <span className='primary_color'>gold backed</span> coin</h2>
-            <h2 className='hero-text mb-10 light_color'>Kinesis is the future of money</h2>
+            <h2 className='hero-text mb-10 primary_color'>Welcome to the Future of Money</h2>
+            <h2 className='hero-text mb-10 light_color'>A new monetary system for a successful and sustainable future</h2>
             <div style={{paddingTop: '30px'}}>
             <ScrollLink style={{ paddingLeft: '12px', paddingRight: '12px'}} className='ml-5 mr-5 mb-20 btn prehighlight' to='contact' spy={true} smooth={true} offset={0} duration={500}>Register Your Interest</ScrollLink>
             </div>
