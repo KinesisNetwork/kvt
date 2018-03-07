4b104de7c1fab72aef2310f56892643afa663e2e
Joel Olsson <joel_1st@hotmail.com>
2018-03-07 12:05:22 +1000

    change revolutionary step to evolutionary step

--- src/components/Promo/What.tsx
+++ src/components/Promo/What.tsx
@@ -15,7 +15,7 @@ export class What extends React.Component<null, null> {
             <div className='section_title text-center mb-60' style={{textAlign: 'left', marginBottom: '-60px'}}>
               <h2 style={{textAlign: 'center'}}>What is Kinesis</h2>
               <p><strong>Kinetically Charged Yield Bearing Asset Based Monetary System of Shared Economic Wealth</strong></p>
-              <p>The Kinesis system is a revolutionary step beyond any current monetary system available in the world today. It enhances money as both a store of value and a medium of exchange, and has been developed for the benefit of all.</p>
+              <p>The Kinesis system is an evolutionary step beyond any current monetary system available in the world today. It enhances money as both a store of value and a medium of exchange, and has been developed for the benefit of all.</p>
               <p>Kinesis is a monetary system which is focused on minimising risk; maximising return; stimulating velocity.  Kinesis is designed to intrinsically promote a rapid rate of adoption.</p>
               <DisplayMore style={{maxWidth: '850px', margin: '0 auto 15px'}}>
                 <p>Core to the mechanics of this monetary system is the perpetual incentive and thus ongoing stimulus for money velocity.  Outside capital is attracted into Kinesis via a highly attractive risk/return ratio and then put into highly stimulated movement, promoting commerce and economic activity.</p>
