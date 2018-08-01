ad69547aed3557fbd5f038cf3bd1e0f4245bf8ec
Sam Jeston <sam.jeston@gmail.com>
2018-08-01 15:43:06 +1000

    Remove deprecated web forms

--- local_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t abxit/kairos-promo .
-docker run -p 9000:80 -v $(pwd):/sync --name kairos --rm -d abxit/kairos-promo
--- src/helpers/ethConversions.ts
+++
@@ -1,7 +0,0 @@
-export function convertEtherToWei (ether) {
-  return ether * 1000000000000000000
-}
-
-export function convertWeiToEther (wei) {
-  return wei / 1000000000000000000
-}
